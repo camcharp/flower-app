79ad6b12a07487198eb4f8bf16ee32305d19c108
Camille Charpentier <cam.charpentier@live.fr>
2022-02-27 17:58:45 +0100

    select seasons - style

--- src/components/FlowersList.vue
+++ src/components/FlowersList.vue
@@ -49,13 +49,24 @@ function formatData(flowers) {
   });
 }
 
+function areAllSeasonsUnchecked(selectedSeasons) {
+  return Object.values(selectedSeasons).every(checked => {
+    return checked === false;
+  });
+}
+
 export default {
   name: 'flower-component',
   data() {
     return {
       flowers: formatData(flowers),
       searchedText: '',
-      selectedSeasons: [],
+      selectedSeasons: {
+        hiver: false,
+        printemps: false,
+        été: false,
+        automne: false,
+      }
     }
   },
   components: {
@@ -66,11 +77,7 @@ export default {
       this.searchedText = value;
     },
     updateSelectedSeasons: function(seasonObject) {
-      if (seasonObject.checked) {
-        this.selectedSeasons.push(seasonObject.season);
-      } else {
-        this.selectedSeasons = this.selectedSeasons.filter(selectedSeason => selectedSeason !== seasonObject.season);
-      }
+      this.selectedSeasons[seasonObject.season] = seasonObject.checked;
     },
     filterFlowers(flowers, input, selectedSeasons) {
       const filteredFlowers = flowers.filter(flower => {
@@ -79,8 +86,11 @@ export default {
         if (input) {
           isFlowerMatchingSearchText = flower.name.toLowerCase().includes(input.toLowerCase());
         }
-        if (selectedSeasons && selectedSeasons.length) {
-          isFlowerMatchinSeasons = flower.seasons.some(season => selectedSeasons.includes(season));
+
+        if (!areAllSeasonsUnchecked(selectedSeasons)) {
+          isFlowerMatchinSeasons = flower.seasons.some(season => {
+            return selectedSeasons[season] === true;
+          });
         }
         return isFlowerMatchingSearchText && isFlowerMatchinSeasons;
       })
@@ -92,7 +102,11 @@ export default {
 
 <template>
   <div class="wrapper">
-    <SearchBar @inputChange="updateSearchedText" @checkbox="updateSelectedSeasons"></SearchBar>
+    <SearchBar 
+      @inputChange="updateSearchedText"
+      @checkbox="updateSelectedSeasons"
+      :seasons="selectedSeasons"
+    ></SearchBar>
     <ul id="flowers">
       <li v-for="flower in filterFlowers(flowers, searchedText, selectedSeasons)" :key="flower.name" class="flower-tile">
         <img class="flower-picture" :src="require(`../assets/flowers/${flower.picture}`)" :alt="flower.name">
@@ -117,4 +131,45 @@ export default {
 </template>
 
 <style scoped>
+  @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:ital@0;1&family=Quicksand:wght@300;400;500;700&display=swap');
+  .wrapper {
+    width: 80%;
+    margin: 0 auto;
+    font-family: 'Quicksand', sans-serif;
+  }
+  #flowers {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
+    align-content: space-between;
+    gap: 2rem 2rem;
+  }
+  ul li {
+    list-style: none;
+  }
+  .flower-picture {
+    width: 100%;
+    height: 60%;
+    border-radius: 55px 55px 0 0;
+  }
+  .flower-tile {
+    width: 350px;
+    height: 500px;
+    border-radius: 55px;
+    background: #eeeded;
+    box-shadow:  12px 12px 27px #dddcdc, -12px -12px 27px #ffffff;    
+  }
+  .flower-title {
+    font-family: 'Playfair Display', serif;
+    font-size: 1.5rem;
+    padding: 0 0 0 2rem;
+  }
+  .flower-seasons, .flower-months {
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+    align-content: space-between;
+    gap: 1rem 1rem;
+  }
 </style>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,11 +1,10 @@
 <script>
 export default {
   name: 'search-bar-component',
-  props: {
-    message: String
-  },
+  props: ['seasons'],
   data() {
     return {
+      message: '',
       value: ''
     }
   },
@@ -28,27 +27,74 @@ export default {
 </script>
 
 <template>
-  <label for="flower-search">
-    Rechercher une fleur :
-  </label>
-  <input
-    :value="message"
-    @input="onInputChange"
-    placeholder="pivoine"
-  >
+  <form id="flower-form">
+    <label for="flower-search">
+      Rechercher une fleur :
+    </label>
+    <input
+      :value="message"
+      @input="onInputChange"
+      placeholder="pivoine"
+    >
+    <label for="hiver" class="season-button">
+      <input type="checkbox" id="hiver" name="hiver" value="hiver" @click="onCheckSeason">
+      hiver
+    </label>
 
-  <input type="checkbox" id="hiver" name="hiver" value="hiver" @click="onCheckSeason">
-  <label for="hiver">hiver</label>
+    <label for="printemps" class="season-button">
+      <input type="checkbox" id="printemps" name="printemps" value="printemps" @click="onCheckSeason">
+      printemps
+    </label>
 
-  <input type="checkbox" id="printemps" name="printemps" value="printemps" @click="onCheckSeason">
-  <label for="printemps">printemps</label>
+    <label for="été" class="season-button">
+      <input type="checkbox" id="été" name="été" value="été" @click="onCheckSeason">
+      été
+    </label>
 
-  <input type="checkbox" id="été" name="été" value="été" @click="onCheckSeason">
-  <label for="été">été</label>
-
-  <input type="checkbox" id="automne" name="automne" value="automne" @click="onCheckSeason">
-  <label for="automne">automne</label>
+    <label for="automne" class="season-button">
+      <input type="checkbox" id="automne" name="automne" value="automne" @click="onCheckSeason">
+      automne
+    </label>
+  </form>
 </template>
 
 <style>
+  #flower-form {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+  }
+  .season-button input[type="checkbox"] {
+    display: none;
+  }
+  .season-button {
+    cursor: pointer;
+    display: inline-block;
+    text-align: center;
+    vertical-align: middle;
+    padding: 12px 24px;
+    border: 1px solid #94636c;
+    border-radius: 8px;
+    background: #ffffff;
+    font: normal normal bold 20px arial;
+    color: black;
+    text-decoration: none;
+}
+  .season-button:hover,
+  .season-button:focus {
+      border: 1px solid #d48e9a;
+      background: #ffc4d4;
+      background: -webkit-gradient(linear, left top, left bottom, from(#ffc4d4), to(#b27782));
+      background: -moz-linear-gradient(top, #ffc4d4, #b27782);
+      background: linear-gradient(to bottom, #ffc4d4, #b27782);
+      color: black;
+      text-decoration: none;
+  }
+  .season-button:active {
+      background: #94636c;
+      background: -webkit-gradient(linear, left top, left bottom, from(#94636c), to(#94636c));
+      background: -moz-linear-gradient(top, #94636c, #94636c);
+      background: linear-gradient(to bottom, #94636c, #94636c);
+  }
+
 </style>
